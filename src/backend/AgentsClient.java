81ebd451a61a97ad06a4692e9fe2da1553716dc6
ParastooB <par_94@yahoo.com>
2017-07-11 20:18:30 -0400

    Trying to make the code only run once on interupt

--- src/backend/AgentsClient.java
+++ src/backend/AgentsClient.java
@@ -9,41 +9,49 @@ public class AgentsClient {
 	public static final int FrameSizeY = 1000;
 
     public static void main(String[] args) {
-        new AgentsClient();
+        //new AgentsClient();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+        } catch (InstantiationException ex) {
+        } catch (IllegalAccessException ex) {
+        } catch (UnsupportedLookAndFeelException ex) {
+        }
+
+        JFrame frame = new JFrame("Spot");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        AgentsGroup agents = new AgentsGroup();
+        frame.add(agents);
+        frame.setSize(FrameSizeX, FrameSizeY);
+        frame.setVisible(true);
+        ThreadsGroup m =  new ThreadsGroup(agents);
+        Agent random = agents.leader();
+        try {
+        	Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+        	System.out.println("didn't sleep for 100 ms");
+        }
+        System.out.println("Start");
+        m.startOne(random);
+        m.interuptOne(random);
+        try {
+        	Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+        	System.out.println("didn't sleep for 100 ms");
+        }
+        m.interuptOne(random);
+        try {
+        	m.waitForOne(random);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("Done");
     }
 
     public AgentsClient() {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (ClassNotFoundException ex) {
-                } catch (InstantiationException ex) {
-                } catch (IllegalAccessException ex) {
-                } catch (UnsupportedLookAndFeelException ex) {
-                }
-
-                JFrame frame = new JFrame("Spot");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                AgentsGroup agents = new AgentsGroup();
-                frame.add(agents);
-                frame.setSize(FrameSizeX, FrameSizeY);
-                frame.setVisible(true);
-                ThreadsGroup m =  new ThreadsGroup(agents);
-                try {
-                	Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                	System.out.println("didn't sleep for 100 ms");
-                	}
-                m.startElection();
-                try {
-                	Thread.sleep(10);
-                } catch (InterruptedException ex) {
-                	System.out.println("didn't sleep for 100 ms");
-                	}
-                m.interuptElection();
             }
         });
     }
--- src/backend/ThreadsGroup.java
+++ src/backend/ThreadsGroup.java
@@ -1,46 +1,58 @@
 package backend;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ThreadsGroup {
 	
-    ThreadGroup tg = new ThreadGroup ("1");
-    ArrayList<Thread> m = new ArrayList<Thread>();
+    private ThreadGroup tg = new ThreadGroup ("1");
+   	private HashMap<Agent,Thread> m = new HashMap<Agent,Thread>();
     
     public ThreadsGroup (AgentsGroup agents){
 	    for (Agent agent : agents.getAgents()) {
 	    	AgentThread gb = new AgentThread(agents,agent,this);
-	        m.add(new Thread(tg, gb, "Thread for " + agent.getAID()));
+	        this.m.put(agent,new Thread(tg, gb, "Thread for " + agent.getAID()));
 	    }
     }
     
     public void startElection(){
-	    for (Thread t : m) {
+	    for (Thread t : m.values()) {
 	    	t.start();
 	    }
     }
     
+    public void startOne(Agent a){
+    	this.m.get(a).start();
+    }
+    
     public void interuptElection(){
-	    for (Thread t : m) {
+	    for (Thread t : m.values()) {
 	    	t.interrupt();
 	    }
     }
     
-    public void endThread(AgentThread e){
-    	m.remove(e);
+    public void interuptOne(Agent a){
+    	this.m.get(a).interrupt();
+    }
+    
+    public void waitForOne(Agent a) throws InterruptedException{
+    	this.m.get(a).join();
+    }
+    
+    public void endThread(Agent e){
+    	this.m.remove(e);
     }
     
     public void stopElection(){
-	    for (Thread t : m) {
+	    for (Thread t : m.values()) {
 	    	t.destroy();
 	    }
     }
     
     public void pauseElection(){
-    	tg.suspend();
+    	//tg.suspend();
     }
     
     public void resumeElection(){
-    	tg.resume();
+    	//g.resume();
     }
 }
